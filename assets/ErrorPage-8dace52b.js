88d9164f23aa177ad7b14083868f9b521afa1274
Valery-Lytvyn <lytvynvalery@gmail.com>
2023-06-13 15:27:41 +0300

    Deploy React app to GitHub Pages

---
+++ assets/ErrorPage-8dace52b.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-31006efe.js";function i(){return e.jsxs("div",{className:"error",children:[e.jsx("h1",{children:"Oops, something went wrong!"}),e.jsx("p",{children:"We are currently experiencing some technical difficulties. Please try again later."})]})}export{i as default};
